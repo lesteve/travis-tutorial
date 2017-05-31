55df85165ceeba9ccafb596fa51e570f008b33b9
Loïc Estève <loic.esteve@ymail.com>
2017-05-31 12:18:37 +0200

    Add np.partition test

--- test_all.py
+++ test_all.py
@@ -1,5 +1,15 @@
+import numpy as np
+
+
 def test_addition():
     assert 1 + 1 == 2
 
+
 def test_division():
     assert 3./2 == 1.5
+
+
+def test_numpy_partition():
+    input_arr = np.array([3, 2, -1, 10])
+    partitioned = np.partition(input_arr, 1)
+    assert partitioned[1] == 2
