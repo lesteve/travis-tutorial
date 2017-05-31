33f15d007b8df625674529c72791f1311804edda
Loïc Estève <loic.esteve@ymail.com>
2017-05-31 10:13:54 +0200

    Add partition wrapper

--- test_all.py
+++ test_all.py
@@ -5,7 +5,14 @@ def test_addition():
     assert 1 + 1 == 2
 
 
+def partition_wrapper(arr, kth):
+    try:
+        return np.partition(arr, kth)
+    except AttributeError:
+        return np.sort(arr)
+
+
 def test_partition():
     input_array = np.array([-10, 3, 1, 5, -7])
-    partitioned_array = np.partition(input_array, 2)
+    partitioned_array = partition_wrapper(input_array, 2)
     assert partitioned_array[2] == 1
