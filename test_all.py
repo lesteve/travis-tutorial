3ece4ccbe747212a00239f7d7ce37b6d5377a157
Loïc Estève <loic.esteve@ymail.com>
2017-05-31 14:39:04 +0200

    Fix Python 2.7 test

--- test_all.py
+++ test_all.py
@@ -8,8 +8,14 @@ def test_addition():
 def test_division():
     assert 3./2 == 1.5
 
+def partition_wrapper(arr, kth):
+    try:
+        return np.partition(arr, kth)
+    except AttributeError:
+        return np.sort(arr)
 
-def test_numpy_partition():
-    input_arr = np.array([3, 2, -1, 10])
-    partitioned = np.partition(input_arr, 1)
-    assert partitioned[1] == 2
+
+def test_partition():
+    input_array = np.array([-10, 3, 1, 5, -7])
+    partitioned_array = partition_wrapper(input_array, 2)
+    assert partitioned_array[2] == 1
