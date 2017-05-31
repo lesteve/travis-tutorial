0b05eb83fee1d3196892d27d93b24c08470a71eb
Loïc Estève <loic.esteve@ymail.com>
2017-05-31 09:52:30 +0200

    Add exercises.md

--- test_all.py
+++ test_all.py
@@ -1,2 +1,11 @@
+import numpy as np
+
+
 def test_addition():
     assert 1 + 1 == 2
+
+
+def test_partition():
+    input_array = np.array([-10, 3, 1, 5, -7])
+    partitioned_array = np.partition(input_array, 2)
+    assert partitioned_array[2] == 1
